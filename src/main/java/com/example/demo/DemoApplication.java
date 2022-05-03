9e59734062df8378eed093527675f57584672434
mer25 <chauqui97@gmail.com>
2022-05-03 17:36:04 +0700

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -50,6 +50,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello world.");
         SpringApplication.run(DemoApplication.class, args);
     }
 
